035c97ecdb9654641ffdbbe549e4f6c8727cc70a
Alex Nichol <unixpickle@gmail.com>
2016-01-18 20:01:33 -0500

    added horizontal line neural net test
    This test is slightly more complex than the firstBitTest(), so I think it better demonstrates the power of a neural net. Unfortunately, it takes ~1 minute to run. Hopefully I can improve that time.

--- neuralnets/main.go
+++ neuralnets/main.go
@@ -7,9 +7,12 @@ import (
 	"time"
 )
 
+const GridSize = 4
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	firstBitTest()
+	horizontalLineTest()
 }
 
 // firstBitTest builds a neural network which outputs 0 for
@@ -41,5 +44,61 @@ func firstBitTest() {
 			encounteredError += math.Abs(desired - output)
 		}
 	}
-	fmt.Println("Average errors:", encounteredError/maxError)
+	fmt.Println("firstBitTest() error rate:", encounteredError/maxError, "want <0.5")
+}
+
+// horizontalLineTest builds a neural network which rejects
+// bitmaps that have horizontal lines.
+func horizontalLineTest() {
+	var maxError float64
+	var encounteredError float64
+
+	for test := 0; test < 10; test++ {
+		network := NewNetwork(GridSize*GridSize, 1, 0, 10, SigmoidFunction{})
+		for i := 0; i < 500000; i++ {
+			input := make([]float64, GridSize*GridSize)
+			for j := range input {
+				input[j] = float64(rand.Intn(2))
+			}
+			desired := 1.0
+			if bitmapHasHorizontal(input) {
+				desired = 0.0
+			}
+			network.SetInput(input)
+			network.Adjust([]float64{desired}, 1)
+		}
+		for i := 0; i < 1000; i++ {
+			maxError += 1
+			input := make([]float64, GridSize*GridSize)
+			for j := range input {
+				input[j] = float64(rand.Intn(2))
+			}
+			desired := 1.0
+			if bitmapHasHorizontal(input) {
+				desired = 0.0
+			}
+			network.SetInput(input)
+			output := network.Evaluate()[0]
+			encounteredError += math.Abs(desired - output)
+		}
+	}
+	probRejected := 1.0 - math.Pow((math.Pow(2, GridSize)-1)/math.Pow(2, GridSize), GridSize)
+	fmt.Println("horizontalLineTest() error rate:", encounteredError/maxError, "want <",
+		probRejected)
+}
+
+func bitmapHasHorizontal(bitmap []float64) bool {
+	for y := 0; y < GridSize; y++ {
+		broken := false
+		for x := 0; x < GridSize; x++ {
+			if bitmap[y*GridSize+x] < 0.5 {
+				broken = true
+				break
+			}
+		}
+		if !broken {
+			return true
+		}
+	}
+	return false
 }

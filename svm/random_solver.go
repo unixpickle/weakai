e5844900c68b0ff5b1c63a78f8bdccc007895325
Alex Nichol <unixpickle@gmail.com>
2016-02-09 20:52:59 -0500

    drafted RandomlySolve() for SVM implementation
    This is as simple as an attempt at a solution gets. I simply "brute force" solutions, picking the best one out of my random attempts. Each attempt combines the sample vectors in random ways, yielding a potential solution.

---
+++ svm/random_solver.go
@@ -0,0 +1,73 @@
+package svm
+
+import (
+	"math"
+	"math/rand"
+)
+
+// RandomlySolve generates many random guesses for solutions and returns the "best" guess,
+// judging based on which guess yields the maximum separation between any two samples.
+func RandomlySolve(p *Problem, numGuesses int, maxWeight float64) *Classifier {
+	var bestClassifier *Classifier
+	var bestSeparation float64
+
+	sampleCount := len(p.Negatives) + len(p.Positives)
+	for i := 0; i < numGuesses; i++ {
+		guess := randomSample(sampleCount, maxWeight)
+		normalVector := make(Sample, len(p.Positives[0]))
+		for i, positive := range p.Positives {
+			coefficient := guess[i]
+			for j, x := range normalVector {
+				normalVector[j] = x + positive[j]
+			}
+		}
+		for i, negative := range p.Negatives {
+			coefficient := guess[i+len(p.Positives)]
+			for j, x := range normalVector {
+				normalVector[j] = x + negative[j]
+			}
+		}
+
+		var minPositiveDot float64
+		var closestPositive Sample
+		for i, positive := range p.Positives {
+			product := p.Kernel(normalVector, positive)
+			if i == 0 || product < minPositiveDot {
+				closestPositive = positive
+				minPositiveDot = product
+			}
+		}
+
+		var maxNegativeDot float64
+		var closestNegative Sample
+		for i, negative := range p.Negatives {
+			product := p.Kernel(normalVector, negative)
+			if i == 0 || product > maxNegativeDot {
+				closestNegative = negative
+				maxNegativeDot = product
+			}
+		}
+
+		separation := (p.Kernel(closestPositive, normalVector) -
+			p.Kernel(closestNegative, normalVector)) /
+			math.Sqrt(p.Kernel(normalVector, normalVector))
+
+		if separation > bestSeparation || i == 0 {
+			bestSeparation = separation
+			bestClassifier = &Classifier{
+				HyperplaneNormal: normalVector,
+				Threshold:        -(minPositiveDot + minNegativeDot) / 2,
+				Kernel:           p.Kernel,
+			}
+		}
+	}
+	return bestClassifier
+}
+
+func randomSample(dimension int, componentMax float64) Sample {
+	res := make(Sample, dimension)
+	for i := range res {
+		res[i] = (rand.Float64() - 0.5) * componentMax * 2
+	}
+	return res
+}

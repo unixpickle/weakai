12a75cdf4f300080b3001b55beb9549476441b23
Alex Nichol <unixpickle@gmail.com>
2016-02-12 18:26:21 -0500

    added cool nonlinear classification demo
    This demo makes use of ascii art (yay!) to show how the classifiers work for a non-linear challenge. Clearly, my gradient descent algorithm is the winner!

---
+++ svm/nonlinear/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/unixpickle/weakai/svm"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	positives := []svm.Sample{
+		{0.5, 0},
+		{0.4, 0.1},
+		{0.3, 0.2},
+		{0.5, 0.9},
+		{0.6, 1},
+		{0.4, 0.85},
+		{0.5, 0.5},
+	}
+
+	negatives := []svm.Sample{
+		{0, 0.5},
+		{0.1, 0.4},
+		{0.9, 0.5},
+		{1, 0.6},
+		{0, 0.46},
+	}
+
+	problem := &svm.Problem{
+		Positives: positives,
+		Negatives: negatives,
+		Kernel:    svm.PolynomialKernel(1, 2),
+	}
+
+	printProblem(problem)
+
+	printSolution("random linear", svm.RandomlySolveLinear(problem, 100000, 5), problem)
+
+	subgradientSolver := &svm.SubgradientSolver{
+		Tradeoff: 0.001,
+		StepSize: 0.1,
+		Steps:    100000,
+	}
+	printSolution("subgradient descent", subgradientSolver.Solve(problem), problem)
+
+	gradientSolver := &svm.GradientDescentSolver{
+		Tradeoff: 0.001,
+		StepSize: 0.001,
+		Steps:    100000,
+	}
+	printSolution("gradient descent", gradientSolver.Solve(problem), problem)
+}
+
+func printProblem(p *svm.Problem) {
+	grid := make([]rune, 10*20)
+	x := func(samples []svm.Sample, char rune) {
+		for _, s := range samples {
+			x, y := int(s[0]*20), int(s[1]*10)
+			if x >= 20 {
+				x = 19
+			}
+			if y >= 10 {
+				y = 9
+			}
+			grid[x+y*20] = char
+		}
+	}
+	x(p.Positives, '+')
+	x(p.Negatives, '-')
+
+	fmt.Println("Problem:")
+	fmt.Println("----------------------")
+	for y := 0; y < 10; y++ {
+		fmt.Print("|")
+		for x := 0; x < 20; x++ {
+			if ch := grid[x+y*20]; ch == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print(string(ch))
+			}
+		}
+		fmt.Println("|")
+	}
+	fmt.Println("----------------------")
+}
+
+func printSolution(solverType string, solution svm.Classifier, p *svm.Problem) {
+	fmt.Println("Got solution from", solverType)
+
+	incorrectCount := 0
+	for _, pos := range p.Positives {
+		if !solution.Classify(pos) {
+			incorrectCount++
+		}
+	}
+	for _, neg := range p.Negatives {
+		if solution.Classify(neg) {
+			incorrectCount++
+		}
+	}
+
+	fmt.Println("Number of incorrect classifications:", incorrectCount)
+
+	fmt.Println("Visual representation:")
+	for y := 0.0; y <= 1.0; y += 0.1 {
+		fmt.Print(" ")
+		for x := 0.0; x < 1.0; x += 0.05 {
+			classification := solution.Classify(svm.Sample{x, y})
+			if classification {
+				fmt.Print("+")
+			} else {
+				fmt.Print("-")
+			}
+		}
+		fmt.Println("")
+	}
+	fmt.Println("")
+}

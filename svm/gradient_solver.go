598dae6eb265fad2744bf72d9d7627cefc406680
Alex Nichol <unixpickle@gmail.com>
2016-05-05 09:36:52 -0400

    created gradientIterator type for later use.

--- svm/gradient_solver.go
+++ svm/gradient_solver.go
@@ -1,6 +1,10 @@
 package svm
 
-import "time"
+import (
+	"time"
+
+	"github.com/unixpickle/num-analysis/linalg"
+)
 
 // A GradientDescentSolver solves Problems using
 // active set gradient descent.
@@ -21,26 +25,96 @@ type GradientDescentSolver struct {
 }
 
 func (c *GradientDescentSolver) Solve(p *Problem) *CombinationClassifier {
-	s := newActiveSetSolver(p, 1/(2*c.Tradeoff))
+	iter := newGradientIterator(p, 1/(2*c.Tradeoff))
 	timeout := time.After(c.Timeout)
 
-	lastValue := s.QuadraticValue()
+	// TODO: this.
 
-StepLoop:
-	for {
-		s.StepGradient()
-		newVal := s.QuadraticValue()
-		if newVal >= lastValue && !s.ConstraintsChanged() {
-			break
+	/*
+			lastValue := .QuadraticValue()
+
+		StepLoop:
+			for {
+				s.StepGradient()
+				newVal := s.QuadraticValue()
+				if newVal >= lastValue && !s.ConstraintsChanged() {
+					break
+				}
+				lastValue = newVal
+
+				select {
+				case <-timeout:
+					break StepLoop
+				default:
+				}
+			}
+
+			return s.Solution(p)
+	*/
+}
+
+type gradientIterator struct {
+	activeSet *activeSet
+
+	// This is A from the quadratic form
+	// 1/2*c'*A*c - b'*c.
+	matrix *linalg.Matrix
+
+	// This is the current approximate solution,
+	// or c from the above quadratic form.
+	solution linalg.Vector
+
+	stepCount          int
+	constraintsChanged bool
+}
+
+func newGradientIterator(p *Problem, maxCoeff float64) *gradientIterator {
+	varCount := len(p.Positives) + len(p.Negatives)
+	posCount := len(p.Positives)
+	signVec := make(linalg.Vector, varCount)
+
+	for i := 0; i < len(p.Positives); i++ {
+		signVec[i] = 1
+	}
+	for i := 0; i < len(p.Negatives); i++ {
+		signVec[i+posCount] = -1
+	}
+
+	res := &gradientIterator{
+		activeSet: newActiveSet(signVec, maxCoeff),
+		matrix:    linalg.NewMatrix(varCount, varCount),
+		solution:  make(linalg.Vector, varCount),
+	}
+
+	for i := 0; i < varCount; i++ {
+		var iSample Sample
+		if i >= posCount {
+			iSample = p.Negatives[i-posCount]
+		} else {
+			iSample = p.Positives[i]
 		}
-		lastValue = newVal
-
-		select {
-		case <-timeout:
-			break StepLoop
-		default:
+		res.matrix.Set(i, i, p.Kernel(iSample, iSample))
+		for j := 0; j < i; j++ {
+			var jSample Sample
+			if j >= posCount {
+				jSample = p.Negatives[j-posCount]
+			} else {
+				jSample = p.Positives[j]
+			}
+			product := p.Kernel(iSample, jSample)
+			res.matrix.Set(i, j, product)
+			res.matrix.Set(j, i, product)
 		}
 	}
 
-	return s.Solution(p)
+	for i := 0; i < varCount; i++ {
+		for j := 0; j < varCount; j++ {
+			oldVal := res.matrix.Get(i, j)
+			oldVal *= signVec[i]
+			oldVal *= signVec[j]
+			res.matrix.Set(i, j, oldVal)
+		}
+	}
+
+	return res
 }

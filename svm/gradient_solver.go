740489e3b5e418b5af580abed8970da6547bdec0
Alex Nichol <unixpickle@gmail.com>
2016-05-05 14:46:33 +0000

    drafted gradientIterator implementation

--- svm/gradient_solver.go
+++ svm/gradient_solver.go
@@ -28,29 +28,25 @@ func (c *GradientDescentSolver) Solve(p *Problem) *CombinationClassifier {
 	iter := newGradientIterator(p, 1/(2*c.Tradeoff))
 	timeout := time.After(c.Timeout)
 
-	// TODO: this.
+	lastValue := iter.QuadraticValue()
 
-	/*
-			lastValue := .QuadraticValue()
+StepLoop:
+	for {
+		iter.StepGradient()
+		newVal := iter.QuadraticValue()
+		if newVal >= lastValue && !iter.ConstraintsChanged() {
+			break
+		}
+		lastValue = newVal
 
-		StepLoop:
-			for {
-				s.StepGradient()
-				newVal := s.QuadraticValue()
-				if newVal >= lastValue && !s.ConstraintsChanged() {
-					break
-				}
-				lastValue = newVal
+		select {
+		case <-timeout:
+			break StepLoop
+		default:
+		}
+	}
 
-				select {
-				case <-timeout:
-					break StepLoop
-				default:
-				}
-			}
-
-			return s.Solution(p)
-	*/
+	return iter.Solution(p)
 }
 
 type gradientIterator struct {
@@ -117,4 +113,118 @@ func newGradientIterator(p *Problem, maxCoeff float64) *gradientIterator {
 	}
 
 	return res
+}
+
+// StepGradient performs a step of constrained
+// gradient descent.
+func (g *gradientIterator) StepGradient() {
+	g.constraintsChanged = false
+	stepDirection := g.gradient().Scale(-1)
+	g.constraintsChanged = g.activeSet.Prune(stepDirection)
+	g.activeSet.ProjectOut(stepDirection)
+	g.step(stepDirection)
+}
+
+// QuadraticValue returns the current value of
+// the minimizing quadratic form.
+func (g *gradientIterator) QuadraticValue() float64 {
+	columnMat := &linalg.Matrix{
+		Rows: g.matrix.Cols,
+		Cols: 1,
+		Data: g.solution,
+	}
+	result := kahan.NewSummer64()
+	quadTerm := columnMat.Transpose().Mul(g.matrix).Mul(columnMat).Data[0]
+	result.Add(quadTerm * 0.5)
+	for _, x := range g.solution {
+		result.Add(-x)
+	}
+	return result.Sum()
+}
+
+// ConstraintsChanged returns whether or not
+// the last step changed any constraints.
+func (g *gradientIterator) ConstraintsChanged() bool {
+	return g.constraintsChanged
+}
+
+// Solution returns the current approximation of
+// the solution.
+func (g *gradientIterator) Solution(p *Problem) *CombinationClassifier {
+	solution := g.solution.Copy()
+
+	// TODO: delete support vectors with 0 coefficients.
+	supportVectors := make([]Sample, len(p.Positives)+len(p.Negatives))
+	copy(supportVectors, p.Positives)
+	copy(supportVectors[len(p.Positives):], p.Negatives)
+	for i := len(p.Positives); i < len(p.Positives)+len(p.Negatives); i++ {
+		solution[i] *= -1
+	}
+
+	res := &CombinationClassifier{
+		SupportVectors: supportVectors,
+		Coefficients:   solution,
+		Kernel:         p.Kernel,
+	}
+	res.computeThreshold(p)
+
+	return res
+}
+
+func (g *gradientIterator) gradient() linalg.Vector {
+	columnMat := &linalg.Matrix{
+		Rows: g.matrix.Cols,
+		Cols: 1,
+		Data: g.solution,
+	}
+	residual := g.matrix.Mul(columnMat)
+	for i, x := range residual.Data {
+		residual.Data[i] = x - 1
+	}
+	return linalg.Vector(residual.Data)
+}
+
+func (g *gradientIterator) optimalStep(d linalg.Vector) float64 {
+	// The optimal step size is (d'*b - c'*A*d)/(d'*A*d)
+	// where d is the direction, A is the matrix, x is
+	// the current approximate solution, and b is all 1's.
+
+	dMat := &linalg.Matrix{
+		Rows: len(d),
+		Cols: 1,
+		Data: d,
+	}
+	ad := linalg.Vector(g.matrix.Mul(dMat).Data)
+
+	summer := kahan.NewSummer64()
+	for _, x := range d {
+		summer.Add(x)
+	}
+
+	numerator := summer.Sum() - g.solution.Dot(ad)
+	denominator := d.Dot(ad)
+
+	return numerator / denominator
+}
+
+func (g *gradientIterator) step(d linalg.Vector) {
+	optimalAmount := g.optimalStep(d)
+	if g.activeSet.step(g.solution, d, optimalAmount) {
+		c.constraintsChanged = true
+	}
+
+	g.stepCount++
+	if g.stepCount%reprojectIterationCount == 0 {
+		g.reprojectConstraints()
+		c.constraintsChanged = true
+	}
+}
+
+func (g *gradientIterator) reprojectConstraints() {
+	for i, x := range g.solution {
+		g.solution[i] = math.Max(0, math.Min(g.activeSet.MaxCoeff, x))
+	}
+	signVec := g.activeSet.SignVec
+	projAmount := signVec.Dot(g.solution) / signVec.Dot(signVec)
+	g.solution.Add(signVec.Copy().Scale(-projAmount))
 }

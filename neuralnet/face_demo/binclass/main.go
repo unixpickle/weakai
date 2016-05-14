9e6742eb7256bf6f9b4951f303d075f578439d3a
Alex Nichol <unixpickle@gmail.com>
2016-05-14 19:47:20 -0400

    fixed binclass arguments

--- neuralnet/face_demo/binclass/main.go
+++ neuralnet/face_demo/binclass/main.go
@@ -9,7 +9,7 @@ func main() {
 	var err error
 	if len(os.Args) == 5 && os.Args[1] == "train" {
 		err = Train(os.Args[2], os.Args[3], os.Args[4])
-	} else if len(os.Args) == 3 && os.Args[1] == "run" {
+	} else if len(os.Args) == 4 && os.Args[1] == "run" {
 		err = Run(os.Args[2], os.Args[3])
 	} else {
 		fmt.Fprintln(os.Stderr, "Usage: binclass train <samples0> <samples1> <classifier-out>\n"+

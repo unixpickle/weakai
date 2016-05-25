fddae8b723b1ef9a9cba89e49d8e04f22a4f9dd4
Alex Nichol <unixpickle@gmail.com>
2016-05-25 10:48:33 -0400

    allow RBM to compute conditional expectations

--- rbm/rbm.go
+++ rbm/rbm.go
@@ -15,13 +15,22 @@ type RBM struct {
 	VisibleBiases linalg.Vector
 }
 
+func NewRBM(visibleCount, hiddenCount int) *RBM {
+	return &RBM{
+		Weights:       linalg.NewMatrix(hiddenCount, visibleCount),
+		HiddenBiases:  make(linalg.Vector, hiddenCount),
+		VisibleBiases: make(linalg.Vector, visibleCount),
+	}
+}
+
 // SampleVisible generates a random visible vector
 // given a vector of hidden layer values.
 // The visible vector will be written to output,
 // allowing the caller to cache a slice for visible
 // samples.
 func (r *RBM) SampleVisible(output, hiddenValues []bool) {
-	r.sample(output, hiddenValues, r.VisibleBiases)
+	expected := r.ExpectedVisible(hiddenValues)
+	sampleVector(output, expected)
 }
 
 // SampleHidden generates a random hidden vector
@@ -30,27 +39,63 @@ func (r *RBM) SampleVisible(output, hiddenValues []bool) {
 // allowing the caller to cache a slice for hidden
 // samples.
 func (r *RBM) SampleHidden(output, visibleValues []bool) {
-	r.sample(output, visibleValues, r.HiddenBiases)
+	expected := r.ExpectedHidden(visibleValues)
+	sampleVector(output, expected)
 }
 
-func (r *RBM) sample(output, input []bool, biases linalg.Vector) {
-	inputCol := &linalg.Matrix{
-		Rows: len(input),
-		Cols: 1,
-		Data: make([]float64, len(input)),
+// ExpectedVisible returns the expected value of
+// the visible layer given a hidden vector.
+func (r *RBM) ExpectedVisible(hidden []bool) linalg.Vector {
+	hiddenRow := &linalg.Matrix{
+		Rows: 1,
+		Cols: len(hidden),
+		Data: make([]float64, len(hidden)),
 	}
-	for i, b := range input {
+	for i, b := range hidden {
 		if b {
-			inputCol.Data[i] = 1
+			hiddenRow.Data[i] = 1
 		}
 	}
-	matrixProduct := r.Weights.Mul(inputCol)
-	weightedInput := linalg.Vector(matrixProduct.Data)
-	energyVec := weightedInput.Add(biases)
+	matrixProduct := hiddenRow.Mul(r.Weights)
+	weighted := linalg.Vector(matrixProduct.Data)
+	expected := weighted.Add(r.VisibleBiases)
 
-	for i, energy := range energyVec {
-		energyExp := math.Exp(energy)
-		prob := energyExp / (1 + energyExp)
+	mapSigmoid(expected)
+
+	return expected
+}
+
+// ExpectedHidden returns the expected value of
+// the hidden layer given a visible vector.
+func (r *RBM) ExpectedHidden(visible []bool) linalg.Vector {
+	visibleCol := &linalg.Matrix{
+		Rows: len(visible),
+		Cols: 1,
+		Data: make([]float64, len(visible)),
+	}
+	for i, b := range visible {
+		if b {
+			visibleCol.Data[i] = 1
+		}
+	}
+	matrixProduct := r.Weights.Mul(visibleCol)
+	weighted := linalg.Vector(matrixProduct.Data)
+	expected := weighted.Add(r.HiddenBiases)
+
+	mapSigmoid(expected)
+
+	return expected
+}
+
+func mapSigmoid(v linalg.Vector) {
+	for i, x := range v {
+		e := math.Exp(x)
+		v[i] = e / (1 + e)
+	}
+}
+
+func sampleVector(output []bool, expected linalg.Vector) {
+	for i, prob := range expected {
 		if rand.Float64() >= prob {
 			output[i] = false
 		} else {

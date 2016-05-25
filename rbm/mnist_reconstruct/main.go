6d8b22d67e29996e03ea48ec02af6a352c7b91b6
Alex Nichol <unixpickle@gmail.com>
2016-05-25 13:43:45 -0400

    created very non-functional mnist RBM demo
    I should probably start testing the RBM code with something simpler than this. So far, this simply outputs a bunch of noise, and doesn't reveal much about where the problem might be.

---
+++ rbm/mnist_reconstruct/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/unixpickle/mnist"
+	"github.com/unixpickle/weakai/rbm"
+)
+
+const (
+	ImageSize  = 28 * 28
+	GibbsSteps = 10
+	StepSize   = 1e-2
+	Epochs     = 100
+
+	ReconstructionGridSize = 5
+)
+
+var HiddenSizes = []int{500, 300, 200}
+
+func main() {
+	training := mnist.LoadTrainingDataSet()
+	samples := make([][]bool, len(training.Samples))
+	for i, sample := range training.Samples {
+		samples[i] = make([]bool, len(sample.Intensities))
+		for j, x := range sample.Intensities {
+			if x > 0.5 {
+				samples[i][j] = true
+			}
+		}
+	}
+
+	layers := buildLayers()
+	trainer := rbm.Trainer{
+		GibbsSteps: GibbsSteps,
+		StepSize:   StepSize,
+		Epochs:     Epochs,
+		BatchSize:  runtime.GOMAXPROCS(0),
+	}
+	log.Println("Training...")
+	trainer.TrainDeep(layers, samples[:1])
+	log.Println("Generating outputs...")
+
+	testingSamples := mnist.LoadTestingDataSet()
+
+	mnist.SaveReconstructionGrid("output.png", func(img []float64) []float64 {
+		return reconstruct(layers, img)
+	}, testingSamples, ReconstructionGridSize, ReconstructionGridSize)
+}
+
+func buildLayers() rbm.DBN {
+	res := make(rbm.DBN, len(HiddenSizes))
+	for i, size := range HiddenSizes {
+		inputSize := ImageSize
+		if i > 0 {
+			inputSize = HiddenSizes[i-1]
+		}
+		res[i] = rbm.NewRBM(inputSize, size)
+	}
+	return res
+}
+
+func reconstruct(dbn rbm.DBN, img []float64) []float64 {
+	binaryInput := make([]bool, len(img))
+	for i, x := range img {
+		if x > 0.5 {
+			binaryInput[i] = true
+		}
+	}
+	output := dbn.Sample(binaryInput)
+	binaryInput = dbn.SampleInput(output)
+	res := make([]float64, len(binaryInput))
+	for i, b := range binaryInput {
+		if b {
+			res[i] = 1
+		}
+	}
+	return res
+}

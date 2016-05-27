354d103f6ee0eb87fb5dbf4029e7e1a0caee8765
Alex Nichol <unixpickle@gmail.com>
2016-05-27 13:51:59 -0400

    benchmark SGD with parallelism

--- neuralnet/training_test.go
+++ neuralnet/training_test.go
@@ -3,6 +3,7 @@ package neuralnet
 import (
 	"math"
 	"math/rand"
+	"runtime"
 	"testing"
 )
 
@@ -52,5 +53,59 @@ func TestTrainingXOR(t *testing.T) {
 		if math.Abs(expected-actual) > 2e-2 {
 			t.Errorf("expected %f for input %v but got %f", expected, sample, actual)
 		}
+	}
+}
+
+func BenchmarkTrainingBigSerial(b *testing.B) {
+	n := runtime.GOMAXPROCS(0)
+	runtime.GOMAXPROCS(1)
+	benchmarkTrainingBig(b)
+	runtime.GOMAXPROCS(n)
+}
+
+func BenchmarkTrainingBigParallel(b *testing.B) {
+	n := runtime.GOMAXPROCS(0)
+	runtime.GOMAXPROCS(3)
+	benchmarkTrainingBig(b)
+	runtime.GOMAXPROCS(n)
+}
+
+func benchmarkTrainingBig(b *testing.B) {
+	inputs := make([][]float64, 100)
+	outputs := make([][]float64, len(inputs))
+	for i := range inputs {
+		inputs[i] = make([]float64, 100)
+		outputs[i] = make([]float64, len(inputs[i]))
+		for j := range inputs[i] {
+			inputs[i][j] = rand.Float64()
+			outputs[i][j] = rand.Float64()
+		}
+	}
+	trainer := &SGD{
+		CostFunc:  MeanSquaredCost{},
+		Inputs:    inputs,
+		Outputs:   outputs,
+		StepSize:  0.01,
+		Epochs:    100,
+		BatchSize: runtime.GOMAXPROCS(0),
+	}
+
+	prototype := []LayerPrototype{
+		&DenseParams{
+			Activation:  Sigmoid{},
+			InputCount:  len(inputs[0]),
+			OutputCount: 50,
+		},
+		&DenseParams{
+			Activation:  Sigmoid{},
+			InputCount:  50,
+			OutputCount: 10,
+		},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		n, _ := NewNetwork(prototype)
+		trainer.Train(n)
 	}
 }

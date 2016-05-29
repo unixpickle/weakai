de8dacdce2ef14c539f15766204f697e2831d12b
Alex Nichol <unixpickle@gmail.com>
2016-05-29 09:44:11 -0400

    created simpler failing LSTM test

--- lstm/rnn_test.go
+++ lstm/rnn_test.go
@@ -20,14 +20,15 @@ var rnnTestOutputs = []linalg.Vector{
 }
 
 const (
-	rnnTestDelta   = 1e-5
-	errorThreshold = 1e-5
+	rnnTestDelta   = 1e-7
+	errorThreshold = 1e-7
 )
 
 type rnnTestCase struct {
 	inputs     []linalg.Vector
 	outputs    []linalg.Vector
 	hiddenSize int
+	sparse     bool
 }
 
 func TestRNNGradientsOneTime(t *testing.T) {
@@ -49,8 +50,9 @@ func TestRNNGradientsThroughTime(t *testing.T) {
 func TestRNNGradientsThroughTimeSingleVar(t *testing.T) {
 	testRNNGradients(t, &rnnTestCase{
 		inputs:     []linalg.Vector{{}, {}},
-		outputs:    []linalg.Vector{{2}, {2}},
+		outputs:    []linalg.Vector{{0.4976425487191876}, {0}},
 		hiddenSize: 1,
+		sparse:     true,
 	})
 }
 
@@ -142,34 +144,39 @@ func rnnForTesting(r *rnnTestCase) *RNN {
 		0.075439, 0.926433, 0.549735, 0.351469, 0.121239,
 		0.415574, 0.094576, 0.727178, 0.858073, 0.758361,
 	}[:outSize*(inSize+hiddenSize)]
-	net.outBiases = linalg.Vector{0.44417, 0.81253}[:outSize]
+	if !r.sparse {
+		net.outBiases = linalg.Vector{0.44417, 0.81253}[:outSize]
+	}
 
 	gateDataLen := hiddenSize * (inSize + hiddenSize)
 
-	net.memoryParams.OutGate.Data = []float64{
-		0.848029, 0.203344, 0.771436, 0.962591, 0.891356,
-		0.023854, 0.462359, 0.778313, 0.580890, 0.625660,
-		0.390262, 0.528941, 0.135359, 0.044588, 0.992219,
-	}[:gateDataLen]
 	net.memoryParams.InGate.Data = []float64{
 		0.404213, 0.359702, 0.168132, 0.631023, 0.088067,
 		0.882302, 0.161765, 0.541852, 0.573962, 0.348698,
 		0.681632, 0.609265, 0.853663, 0.462425, 0.709695,
 	}[:gateDataLen]
-	net.memoryParams.RemGate.Data = []float64{
-		0.108275, 0.493979, 0.542174, 0.414007, 0.400178,
-		0.780442, 0.061278, 0.052919, 0.842510, 0.953911,
-		0.961888, 0.648135, 0.852180, 0.562492, 0.978203,
-	}[:gateDataLen]
-	net.memoryParams.InWeights.Data = []float64{
-		0.062555, 0.903507, 0.986772, 0.375239, 0.610480,
-		0.894272, 0.257381, 0.025877, 0.368605, 0.823952,
-		0.666342, 0.250000, 0.544260, 0.147394, 0.269642,
-	}[:gateDataLen]
 
-	net.memoryParams.InGateBiases = linalg.Vector{0.576392, 0.431650, 0.090567}[:hiddenSize]
-	net.memoryParams.OutGateBiases = linalg.Vector{0.52432, 0.25997, 0.59234}[:hiddenSize]
-	net.memoryParams.RemGateBiases = linalg.Vector{0.62371, 0.81187, 0.51603}[:hiddenSize]
-	net.memoryParams.InBiases = linalg.Vector{0.85035, 0.56379, 0.44774}[:hiddenSize]
+	if !r.sparse {
+		net.memoryParams.OutGate.Data = []float64{
+			0.848029, 0.203344, 0.771436, 0.962591, 0.891356,
+			0.023854, 0.462359, 0.778313, 0.580890, 0.625660,
+			0.390262, 0.528941, 0.135359, 0.044588, 0.992219,
+		}[:gateDataLen]
+		net.memoryParams.RemGate.Data = []float64{
+			0.108275, 0.493979, 0.542174, 0.414007, 0.400178,
+			0.780442, 0.061278, 0.052919, 0.842510, 0.953911,
+			0.961888, 0.648135, 0.852180, 0.562492, 0.978203,
+		}[:gateDataLen]
+		net.memoryParams.InWeights.Data = []float64{
+			0.062555, 0.903507, 0.986772, 0.375239, 0.610480,
+			0.894272, 0.257381, 0.025877, 0.368605, 0.823952,
+			0.666342, 0.250000, 0.544260, 0.147394, 0.269642,
+		}[:gateDataLen]
+
+		net.memoryParams.InGateBiases = linalg.Vector{0.576392, 0.431650, 0.090567}[:hiddenSize]
+		net.memoryParams.OutGateBiases = linalg.Vector{0.52432, 0.25997, 0.59234}[:hiddenSize]
+		net.memoryParams.RemGateBiases = linalg.Vector{0.62371, 0.81187, 0.51603}[:hiddenSize]
+		net.memoryParams.InBiases = linalg.Vector{0.85035, 0.56379, 0.44774}[:hiddenSize]
+	}
 	return net
 }

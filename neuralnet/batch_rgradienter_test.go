7d0d74d1443ff304805e8ac1005105d5d63c5f7a
Alex Nichol <unixpickle@gmail.com>
2016-06-10 18:18:51 -0400

    test BatchRGradienter more directly
    Before now, the only test for BatchRGradienter was with the XOR tests. These did reveal some problems on occasion, but the new tests are much more reliable, since they actual make sure the gradient is correct.

---
+++ neuralnet/batch_rgradienter_test.go
@@ -0,0 +1,130 @@
+package neuralnet
+
+import (
+	"math"
+	"math/rand"
+	"runtime"
+	"testing"
+
+	"github.com/unixpickle/autofunc"
+	"github.com/unixpickle/num-analysis/linalg"
+)
+
+const (
+	batchRGradienterSeed     = 123123
+	batchRGradienterTestPrec = 1e-5
+)
+
+func TestBatchRGradienterSimple(t *testing.T) {
+	testBatchRGradienter(t, 1, &BatchRGradienter{
+		CostFunc:      MeanSquaredCost{},
+		MaxGoroutines: 1,
+		MaxBatchSize:  1,
+	})
+}
+
+func TestBatchRGradienterUnevenBatch(t *testing.T) {
+	testBatchRGradienter(t, 16, &BatchRGradienter{
+		CostFunc:      MeanSquaredCost{},
+		MaxGoroutines: 1,
+		MaxBatchSize:  3,
+	})
+}
+
+func TestBatchRGradienterUnevenConcurrent(t *testing.T) {
+	n := runtime.GOMAXPROCS(0)
+	runtime.GOMAXPROCS(8)
+	testBatchRGradienter(t, 16, &BatchRGradienter{
+		CostFunc:      MeanSquaredCost{},
+		MaxGoroutines: 8,
+		MaxBatchSize:  1,
+	})
+	runtime.GOMAXPROCS(n)
+}
+
+func testBatchRGradienter(t *testing.T, batchSize int, b *BatchRGradienter) {
+	rand.Seed(batchRGradienterSeed)
+
+	net := Network{
+		&DenseLayer{
+			InputCount:  10,
+			OutputCount: 30,
+		},
+		&Sigmoid{},
+		&DenseLayer{
+			InputCount:  30,
+			OutputCount: 3,
+		},
+		&Sigmoid{},
+	}
+	net.Randomize()
+	b.Learner = net
+
+	samples := &SampleSet{}
+	samples.Inputs = make([]linalg.Vector, batchSize)
+	samples.Outputs = make([]linalg.Vector, batchSize)
+	for i := range samples.Inputs {
+		inputVec := make(linalg.Vector, 10)
+		outputVec := make(linalg.Vector, 3)
+		for j := range inputVec {
+			inputVec[j] = rand.NormFloat64()
+		}
+		for j := range outputVec {
+			outputVec[j] = rand.Float64()
+		}
+		samples.Inputs[i] = inputVec
+		samples.Outputs[i] = outputVec
+	}
+
+	rVector := autofunc.RVector(autofunc.NewGradient(net.Parameters()))
+	for _, vec := range rVector {
+		for i := range vec {
+			vec[i] = rand.NormFloat64()
+		}
+	}
+
+	single := SingleRGradienter{Learner: net, CostFunc: b.CostFunc}
+	expectedGrad := single.Gradient(samples)
+	actualGrad := b.Gradient(samples)
+
+	if !vecMapsEqual(expectedGrad, actualGrad) {
+		t.Error("bad gradient from Gradient()")
+	}
+
+	expectedGrad, expectedRGrad := single.RGradient(rVector, samples)
+	actualGrad, actualRGrad := b.RGradient(rVector, samples)
+
+	if !vecMapsEqual(expectedGrad, actualGrad) {
+		t.Error("bad gradient from RGradient()")
+	}
+	if !vecMapsEqual(expectedRGrad, actualRGrad) {
+		t.Error("bad r-gradient from RGradient()")
+	}
+}
+
+func vecMapsEqual(m1, m2 map[*autofunc.Variable]linalg.Vector) bool {
+	for k := range m1 {
+		if _, ok := m2[k]; !ok {
+			return false
+		}
+	}
+	for k := range m2 {
+		if _, ok := m1[k]; !ok {
+			return false
+		}
+	}
+
+	for k, v := range m1 {
+		v1 := m2[k]
+		if len(v) != len(v1) {
+			return false
+		}
+		for i, x := range v {
+			if math.Abs(x-v1[i]) > batchRGradienterTestPrec {
+				return false
+			}
+		}
+	}
+
+	return true
+}

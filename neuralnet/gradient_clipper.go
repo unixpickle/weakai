afc0e3e566ef8b181b549efa5b77f3e2770f521e
Alex Nichol <unixpickle@gmail.com>
2016-06-21 11:02:03 -0400

    take different norms in GradientClipper

--- neuralnet/gradient_clipper.go
+++ neuralnet/gradient_clipper.go
@@ -6,23 +6,40 @@ import (
 	"github.com/unixpickle/autofunc"
 )
 
+type GradientNorm int
+
+const (
+	L2Norm GradientNorm = iota
+	InfNorm
+)
+
 // GradientClipper is a Gradienter which scales down
-// gradients so that the L2 norm of the gradient is
-// less than a certain value.
+// gradients so that the norm of the gradient is less
+// than a certain value.
 type GradientClipper struct {
 	Gradienter Gradienter
 	Threshold  float64
+	Norm       GradientNorm
 }
 
 func (c *GradientClipper) Gradient(s SampleSet) autofunc.Gradient {
 	res := c.Gradienter.Gradient(s)
-	var magnitude float64
-	for _, vec := range res {
-		magnitude += vec.Dot(vec)
+	var norm float64
+
+	switch c.Norm {
+	case L2Norm:
+		for _, vec := range res {
+			norm += vec.Dot(vec)
+		}
+		norm = math.Sqrt(norm)
+	case InfNorm:
+		for _, vec := range res {
+			norm = math.Max(norm, vec.MaxAbs())
+		}
 	}
-	magnitude = math.Sqrt(magnitude)
-	if magnitude > c.Threshold {
-		res.Scale(c.Threshold / magnitude)
+
+	if norm > c.Threshold {
+		res.Scale(c.Threshold / norm)
 	}
 	return res
 }

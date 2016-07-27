f121755115c11babfa0779369fcb54121390cbb1
Alex Nichol <unixpickle@gmail.com>
2016-07-26 21:47:50 -0400

    drafted squared loss for gradient boosting

---
+++ boosting/loss_func.go
@@ -0,0 +1,41 @@
+package boosting
+
+import (
+	"github.com/unixpickle/autofunc"
+	"github.com/unixpickle/num-analysis/linalg"
+)
+
+// A LossFunc is a differentiable function which can
+// measure the difference between two classification
+// vectors.
+type LossFunc interface {
+	// Loss evaluates the loss function to find the
+	// distance between the actual classification and
+	// the expected (desired) classification.
+	// The resulting autofunc.Result should contain
+	// one component: a scaler measure of the error.
+	Loss(actual autofunc.Result, expected linalg.Vector) autofunc.Result
+
+	// OptimalStep returns the value a which minimizes
+	// Loss(x0+a*x, y).
+	// In other words, it performs a line search to
+	// find the optimal step size.
+	OptimalStep(x0, x, y linalg.Vector) float64
+}
+
+// SquareLoss is a loss function which squares the
+// distances between desired and actual classifications.
+type SquareLoss struct{}
+
+// Loss returns the squared magnitude of the difference
+// between actual and expected.
+func (_ SquareLoss) Loss(actual autofunc.Result, expected linalg.Vector) autofunc.Result {
+	expVar := &autofunc.Variable{Vector: expected.Copy().Scale(-1)}
+	return autofunc.SumAll(autofunc.Square(autofunc.Add(actual, expVar)))
+}
+
+// OptimalStep returns the value a which minimizes
+// the squared distance between y and x0+a*x.
+func (_ SquareLoss) OptimalStep(x0, x, y linalg.Vector) float64 {
+	return (x.Dot(y) - x.Dot(x0)) / x.Dot(x)
+}

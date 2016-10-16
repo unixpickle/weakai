49b8f56a95566b661f069c8f5ddb68b480db0cea
Alex Nichol <unixpickle@gmail.com>
2016-10-15 21:11:01 -0400

    drafted new block API

--- rnn/block.go
+++ rnn/block.go
@@ -5,103 +5,88 @@ import (
 	"github.com/unixpickle/num-analysis/linalg"
 )
 
-// A Block is a unit in a Recurrent Neural Network that
-// transforms input-state pairs into output/state pairs.
+// A Block is a differentiable unit in a recurrent model
+// that transforms input/state pairs into output/state
+// pairs.
 type Block interface {
-	// StateSize returns the number of values in each
-	// state of the Block.
-	StateSize() int
+	// StartState returns the initial state for the Block.
+	StartState() State
 
-	// StartState returns the initial state for starting
-	// new sequences with this block.
-	// This is an autofunc.Result because it may be
-	// back-propagated through during training.
-	StartState() autofunc.Result
+	// StartStateR is like StartState but for an RState.
+	StartRState(rv autofunc.RVector) RState
 
-	// StartStateR is like StartState but with r-operator
-	// support.
-	StartStateR(rv autofunc.RVector) autofunc.RResult
+	// PropagateStart performs back-propagation through the
+	// start state.
+	PropagateStart(s StateGrad, g autofunc.Gradient)
 
-	// Batch applies forward propagation to a BlockInput.
-	// The result is valid so long as neither the input
-	// nor the Block is changed.
-	Batch(in *BlockInput) BlockOutput
+	// PropagateStartR is like PropagateStart, but for an
+	// RStateGrad.
+	PropagateStartR(r RStateGrad, rg autofunc.RGradient, g autofunc.Gradient)
 
-	// BatchR is like Batch, but for an BlockRInput.
-	// The result is valid so long as neither the input
-	// nor the Block is changed.
+	// ApplyBlock applies the block to a batch of inputs.
+	// The result is valid so long as neither the inputs
+	// nor the Block are changed.
+	ApplyBlock(s State, in []autofunc.Result) BlockResult
+
+	// ApplyBlockR is like ApplyBlock, but with support for
+	// the R operator.
 	//
 	// It is necessary to provide an RVector so that the
 	// block knows how much each of its hidden parameters
 	// changes with respect to R.
-	BatchR(v autofunc.RVector, in *BlockRInput) BlockROutput
+	ApplyBlockR(v autofunc.RVector, s RState, in []autofunc.RResult) BlockROutput
 }
 
-// UpstreamGradient stores the gradients of some
-// output with respect to the outputs and output
-// states of some Block.
-// Either one of the slices (States or Outputs)
-// may be nil, indicating that said gradient is
-// completely 0.
-type UpstreamGradient struct {
-	States  []linalg.Vector
-	Outputs []linalg.Vector
-}
+// A State represents some internal, recurrent state.
+// For instance, it might represent the current contents
+// of LSTM blocks.
+type State interface{}
 
-// UpstreamRGradient is like UpstreamGradient,
-// but it stores the derivatives of all the
-// partials with respect to some variable R.
-//
-// A slice (States or Outputs) can be nil if and
-// only if its corresponding R slice is also nil.
-type UpstreamRGradient struct {
-	UpstreamGradient
-	RStates  []linalg.Vector
-	ROutputs []linalg.Vector
-}
+// A StateGrad represents the gradient of a loss value
+// with respect to a given State.
+type StateGrad interface{}
 
-// A BlockInput stores a batch of states and inputs
-// for a Block.
-type BlockInput struct {
-	States []*autofunc.Variable
-	Inputs []*autofunc.Variable
-}
+// An RState is like a State, but with information about
+// second derivatives with respect to some variable R.
+type RState interface{}
 
-// A BlockOutput represents a batch of outputs and new
-// states from a Block.
-type BlockOutput interface {
-	States() []linalg.Vector
+// An RStateGrad is like a StateGrad, but for an RState.
+type RStateGrad interface{}
+
+// A BlockResult represents the output of a Block.
+type BlockResult interface {
+	// Outputs returns the vector outputs of the block.
 	Outputs() []linalg.Vector
 
-	// Gradient updates the gradients in g given the
-	// upstream gradient from this BlockOutput.
-	// This should not modify u.
-	Gradient(u *UpstreamGradient, g autofunc.Gradient)
+	// State returns the new state of the block.
+	State() State
+
+	// PropagateGradient performs back-propagation through
+	// the block and the inputs.
+	// It returns the downstream gradient with respect to the
+	// previous state.
+	// Either upstream or s may be nil, but not both.
+	PropagateGradient(upstream []linalg.Vector, s StateGrad, g autofunc.Gradient) StateGrad
 }
 
-// A BlockRInput is like a BlockInput, but includes
-// derivatives of all the inputs and states with
-// respect to some variable R.
-type BlockRInput struct {
-	States []*autofunc.RVariable
-	Inputs []*autofunc.RVariable
-}
-
-// An BlockROutput is like a BlockOutput, but includes
-// derivatives of the outputs and states with respect
-// to some variable R.
-type BlockROutput interface {
-	States() []linalg.Vector
+// A BlockRResult is like a BlockResult, but for RStates.
+type BlockRResult interface {
+	// Outputs returns the vector outputs of the block.
 	Outputs() []linalg.Vector
-	RStates() []linalg.Vector
+
+	// ROutputs returns the derivative of Outputs() with
+	// respect to R.
 	ROutputs() []linalg.Vector
 
-	// RGradient updates the gradients in g and the
-	// r-gradients in rg given the upstream gradient
-	// u and the derivative of u with respect to R,
-	// stored in ru.
-	// The gradient g may be nil to indicate that only
-	// the r-gradient is desired.
-	// This should not modify u.
-	RGradient(u *UpstreamRGradient, rg autofunc.RGradient, g autofunc.Gradient)
+	// RState returns the new state of the block.
+	RState() RState
+
+	// PropagateRGradient performs back-propagation through
+	// the block and the inputs.
+	// It returns the downstream gradient with respect to the
+	// previous state.
+	// Either upstream or s may be nil, but not both.
+	// For convenience, g may be nil.
+	PropagateRGradient(upstream []linalg.Vector, s RStateGrad, rg autofunc.RGradient,
+		g autofunc.Gradient) RStateGrad
 }

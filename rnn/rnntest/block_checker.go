fafc1f113e7fcb8c5eeed4554ca594075cd75f2a
Alex Nichol <unixpickle@gmail.com>
2016-10-20 18:10:08 -0400

    created utility to prevent test-seq reuse

--- rnn/rnntest/block_checker.go
+++ rnn/rnntest/block_checker.go
@@ -2,11 +2,13 @@ package rnntest
 
 import (
 	"math"
+	"math/rand"
 	"testing"
 
 	"github.com/unixpickle/autofunc"
 	"github.com/unixpickle/autofunc/functest"
 	"github.com/unixpickle/num-analysis/linalg"
+	"github.com/unixpickle/sgd"
 	"github.com/unixpickle/weakai/rnn"
 )
 
@@ -34,6 +36,42 @@ type BlockChecker struct {
 	// Prec is the precision to use when comparing values.
 	// If it is 0, functest.DefaultPrec is used.
 	Prec float64
+}
+
+// NewChecker4In creates a BlockChecker which feeds a
+// block a few input sequences whose input vectors contain
+// four values each.
+func NewChecker4In(b rnn.Block, l sgd.Learner) *BlockChecker {
+	testVars := []*autofunc.Variable{
+		{Vector: []float64{0.098591, -0.595453, -0.751214, 0.266051}},
+		{Vector: []float64{0.988517, 0.107284, -0.331529, 0.028565}},
+		{Vector: []float64{-0.150604, 0.889039, 0.120916, 0.240999}},
+		{Vector: []float64{0.961058, 0.878608, 0.052284, -0.635746}},
+	}
+	testSeqs := [][]*autofunc.Variable{
+		{testVars[0], testVars[2]},
+		{testVars[1]},
+		{testVars[2], testVars[1], testVars[3]},
+	}
+	testRV := autofunc.RVector{
+		testVars[0]: []float64{0.62524, -0.52979, 0.33020, 0.54462},
+		testVars[1]: []float64{0.13498, 0.12607, 0.35989, 0.23255},
+		testVars[2]: []float64{0.85996, 0.68435, -0.68506, 0.96907},
+		testVars[3]: []float64{-0.79095, -0.33867, 0.86759, -0.16159},
+	}
+	for _, v := range l.Parameters() {
+		testVars = append(testVars, v)
+		testRV[v] = make(linalg.Vector, len(v.Vector))
+		for i := range v.Vector {
+			testRV[v][i] = rand.NormFloat64()
+		}
+	}
+	return &BlockChecker{
+		B:     b,
+		Input: testSeqs,
+		Vars:  testVars,
+		RV:    testRV,
+	}
 }
 
 // FullCheck performs gradient checking and checks  for
--- rnn/rnntest/state_out_block_test.go
+++ rnn/rnntest/state_out_block_test.go
@@ -9,23 +9,6 @@ import (
 )
 
 func TestStateOutBlock(t *testing.T) {
-	testVars := []*autofunc.Variable{
-		{Vector: []float64{0.098591, -0.595453, -0.751214, 0.266051}},
-		{Vector: []float64{0.988517, 0.107284, -0.331529, 0.028565}},
-		{Vector: []float64{-0.150604, 0.889039, 0.120916, 0.240999}},
-		{Vector: []float64{0.961058, 0.878608, 0.052284, -0.635746}},
-	}
-	testSeqs := [][]*autofunc.Variable{
-		{testVars[0], testVars[2]},
-		{testVars[1]},
-		{testVars[2], testVars[1], testVars[3]},
-	}
-	testRV := autofunc.RVector{
-		testVars[0]: []float64{0.62524, -0.52979, 0.33020, 0.54462},
-		testVars[1]: []float64{0.13498, 0.12607, 0.35989, 0.23255},
-		testVars[2]: []float64{0.85996, 0.68435, -0.68506, 0.96907},
-		testVars[3]: []float64{-0.79095, -0.33867, 0.86759, -0.16159},
-	}
 	net := neuralnet.Network{
 		&neuralnet.DenseLayer{
 			InputCount:  8,
@@ -34,14 +17,20 @@ func TestStateOutBlock(t *testing.T) {
 		&neuralnet.HyperbolicTangent{},
 	}
 	net.Randomize()
+	startVar := &autofunc.Variable{Vector: []float64{0.3, -0.3, 0.2, 0.5}}
 	block := &rnn.StateOutBlock{
-		Block: &rnn.BatcherBlock{B: net.BatchLearner(), StateSize: 4, Start: testVars[2]},
+		Block: &rnn.BatcherBlock{
+			B:         net.BatchLearner(),
+			StateSize: 4,
+			Start:     startVar,
+		},
 	}
-	checker := &BlockChecker{
-		B:     block,
-		Input: testSeqs,
-		Vars:  testVars,
-		RV:    testRV,
-	}
-	checker.FullCheck(t)
+	learner := append(stateOutBlockLearner{startVar}, net.Parameters()...)
+	NewChecker4In(block, learner).FullCheck(t)
+}
+
+type stateOutBlockLearner []*autofunc.Variable
+
+func (s stateOutBlockLearner) Parameters() []*autofunc.Variable {
+	return s
 }
